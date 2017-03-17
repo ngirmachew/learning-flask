264bfbf827d61a597a50bc5060d5df47782738bf
ngirmachew <negailbsg@gmail.com>
2017-03-17 06:10:03 -0400

    Create signup form - front and back, modify routes accordingly.

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
