ef085f4a39de67bac8929d333b866749f6ea1411
ngirmachew <negailbsg@gmail.com>
2017-03-18 11:00:26 -0400

    Install psycopg2, amend requirements, fix config in routes.py for 'SQLALCHEMY_DATABASE_URI'

--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask' ->NGL - Changed to below because of the error: "Flask sqlalchemy.exc.OperationalError: (psycopg2.OperationalError) FATAL: role does not exist"
+# Solution source: "http://www.bogotobogo.com/python/Flask/Python_Flask_App_0_Word_Count_Errors_Fixes.php"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/learningflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #->Save resources use
 db.init_app(app)
 
@@ -26,6 +28,9 @@ def signup():
         if form.validate() == False:
             return render_template('signup.html', form = form)
         else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
             return "Success!"
 			
     elif request.method == 'GET':
