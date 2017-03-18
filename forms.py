426198ae837c83e60f3ce94290277cb63ba4db1f
ngirmachew <negailbsg@gmail.com>
2017-03-18 03:17:36 -0400

    Add custom/user-friendly error messages to form.py

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-    first_name = StringField('First name', validators = [DataRequired()])
-    last_name = StringField('Last name', validators = [DataRequired()])
-    email = StringField('Email', validators = [DataRequired()])
-    password = PasswordField('Password', validators = [DataRequired()])
+    first_name = StringField('First name', validators = [DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators = [DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators = [DataRequired("Please enter your email address.")])
+    password = PasswordField('Password', validators = [DataRequired("Please enter your password.")])
     submit = SubmitField('Sign up')
